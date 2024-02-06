944f7ee3a5f9887aee77f2561df24cad6c593f97
Gonzalez Agustin <agusgonzalez603@hotmail.com>
2024-02-06 17:14:34 -0300

    Juego terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule, NgIf } from '@angular/common'; // Importa NgIf
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'DadosAngular';
+  dadoIzquierda = '../assets/img/dice1.png';
+  dadoDerecha = '../assets/img/dice6.png';
+  numero1: number = 1;
+  numero2: number = 6;
+
+  tirarDados(): void{
+    this.numero1 = Math.floor(Math.random() * 6) + 1;
+    this.numero2 = Math.floor(Math.random() * 6) + 1;
+    this.dadoIzquierda = '../assets/img/dice' + this.numero1 + '.png';
+    this.dadoDerecha = '../assets/img/dice' + this.numero2 + '.png';
+  } 
 }
